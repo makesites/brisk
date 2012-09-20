1c9785850ce2165c670bed31083dea0640b9819b
Makis Tracend <makis.tracend@gmail.com>
2012-09-20 13:35:57 -0700

    Enabling class inheritance for controllers

--- controllers/main.js
+++ controllers/main.js
@@ -1,18 +1,18 @@
+var Class = require("../helpers/class")
 
-main = function(){
-	
-	
-}
-
-main.prototype.init = function(req, res){
-	// logic...
-	res.template = "main";
-	res.view = "init";
-	// render 
-	//console.log( this );
-	//this.render(res);
-	res.send("here");
-}
+main = Class.extend({
+  init: function(req, res){
+		// db initialiazation
+		//...
+		// logic...
+		res.template = "main";
+		res.view = "init";
+		// render 
+		//console.log( this );
+		//this.render(res);
+		res.send("here");
+	}
+});
 
 main.prototype.render = function(res){
 	console.log( path.join(__dirname, '/views/'+res.template) );
