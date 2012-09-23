f09da09341612024d391b52ec307b7bafd5756b9
Makis Tracend <makis.tracend@gmail.com>
2012-09-22 18:00:58 -0700

    Minor tweak on the render call

--- controllers/main.js
+++ controllers/main.js
@@ -12,11 +12,14 @@ main = Class.extend({
 		//this.render(res);
 		res.send("here");
 	}
+	
 });
 
 main.prototype.render = function(res){
-	console.log( path.join(__dirname, '/views/'+res.template) );
-	res.render(res.view, { layout: path.join(__dirname, '/views/'+res.template) });
+	//console.log( path.join(__dirname, '/views/'+res.template) );
+	//res.render(res.view, { layout: path.join(__dirname, '/views/'+res.template) });
+	res.render(res.view);
 }
+
 
 module.exports = main;
