43466080d65f73ba56116ac5e86a3b27661c3433
Makis Tracend <makis.tracend@gmail.com>
2012-10-04 16:14:22 -0700

    Changing the way the configuration is parsed

--- config/app.js
+++
@@ -1,6 +0,0 @@
-var path = require("path");
-
-module.exports = {
-	root : path.normalize(__dirname + "/../"), 
-	controllers : { main : "main" }
-}
---
+++ config/default.js
@@ -0,0 +1,10 @@
+var path = require("path");
+
+module.exports = {
+	//root : path.normalize(__dirname + "/../"), 
+	
+	defaults: {
+       "controller": "main" 
+    }
+    
+}
