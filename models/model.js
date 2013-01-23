28a96576a96a3565a79725680dfd561bc8caf0d7
Makis Tracend <makis.tracend@gmail.com>
2013-01-23 12:40:20 -0800

    #19 adding default model

---
+++ models/model.js
@@ -0,0 +1,44 @@
+var Main = require("brisk").getClass("main");  
+
+var model = Main.extend({
+	init: function( site ){ 
+		// db
+		this.db = site.modules.db;
+		
+	}, 
+	
+	schema : function(){
+		return {}
+	}, 
+	
+	get: function( key ) {
+		
+		return (typeof this.data[key] != "undefined") ? this.data[key] : null;
+
+    }, 
+	set: function( data ) {
+		
+		for( var i in data ){
+			// only update the model with set values
+			if(typeof this.data[i] != "undefined"){
+				this.data[i] = data[i]; // clean up the data first?
+			}
+		}
+
+    }, 
+    create: function( data, callback ) {
+
+    }, 
+    read: function( data, callback, options ) {
+		
+    }, 
+    update: function( data, callback ) {
+		
+    }, 
+    delete: function( data, callback ) {
+		
+    }
+		
+});
+
+module.exports = model;
