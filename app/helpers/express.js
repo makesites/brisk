4da7b9444d38944da8ef353f5dda209e5175916d
Makis Tracend <makis.tracend@gmail.com>
2013-04-03 04:29:04 -0700

    #5 including base helper for express that contains the cors method

---
+++ app/helpers/express.js
@@ -0,0 +1,20 @@
+var brisk = require("brisk"), 
+	Parent = require("./class"),
+	CORS = brisk.getLib("connect-cors");
+
+var helper = Parent.extend({
+	
+	//CORS middleware
+	cors : function() {
+		// load configuration 
+		return CORS( brisk.loadConfig('cors') );
+	}, 
+	
+	self: function() {
+		return this.express;
+	}
+	
+});
+
+
+module.exports = helper;
