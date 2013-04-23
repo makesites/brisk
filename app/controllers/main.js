acced4a5cd7140d199d85ed330fc9536ed1404c9
Makis Tracend <makis.tracend@gmail.com>
2013-04-23 01:52:00 -0700

    reverting from using options for the 'authenticated' flag

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -4,8 +4,7 @@ var Class = require("../helpers/class"),
 main = Class.extend({
 	// defaults
 	options: {
-		debug : DEV, 
-		authenticated : null 
+		debug : DEV
 	}, 
 	
 	index: function(req, res){
@@ -17,9 +16,9 @@ main = Class.extend({
 	},
 	
 	render : function(req, res){
-		
+		var self = this;
 		// get authentication status
-		res.locals({ authenticated : this.isAuthenticated( req, res ) });
+		res.locals.authenticated = res.locals.authenticated || req.isAuthenticated();
 		// access the user session in the views 
 		if( typeof req.user != "undefined" ){
 			res.locals({ user : req.user });
@@ -29,7 +28,7 @@ main = Class.extend({
 		res.render(res.view, null, function(err, result) {
 			//console.log('Render result:', result);
 			// compact output by removing carriage returns, tabs and extra whitespace
-			var html = (this.options.debug) ? result : result.replace(/(\r\n|\n|\r|\t)/gm,"").replace(/\s+/gm, " ");
+			var html = (self.options.debug) ? result : result.replace(/(\r\n|\n|\r|\t)/gm,"").replace(/\s+/gm, " ");
 			res.send(html); // send rendered HTML back to client
 		})
 	}, 
@@ -44,20 +43,16 @@ main = Class.extend({
 // Helpers
 
 main.prototype.ensureAuthenticated = function(req, res, next) {
-	if( this.options.authenticated === null ){
-		this.options.authenticated = req.isAuthenticated();
-	}
-	if ( this.options.authenticated ) { return (next) ? next() : true; }
+	res.locals.authenticated = res.locals.authenticated || req.isAuthenticated();
+	if ( res.locals.authenticated ) { return (next) ? next() : true; }
 	// always redirect to the homepage if not authenticated (customize?)
 	res.redirect('/');
 	return false;
 };
 
 main.prototype.isAuthenticated = function(req, res) {
-	if( this.options.authenticated === null ){
-		this.options.authenticated = req.isAuthenticated();
-	}
-	return this.options.authenticated;
+	res.locals.authenticated = res.locals.authenticated || req.isAuthenticated();
+	return res.locals.authenticated;
 };
 
 module.exports = main;
