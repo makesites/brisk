ae2a71948217c48e82d8f017d13e2c2e088ac6d8
Makis Tracend <makis.tracend@gmail.com>
2013-04-23 00:42:51 -0700

    #30 adding request to render

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -15,6 +15,16 @@ main = Class.extend({
 		//this.render(res);
 	},
 	
+	render : function(req, res){
+		//console.log( path.join(__dirname, '/views/'+res.template) );
+		//res.render(res.view, { layout: path.join(__dirname, '/views/'+res.template) });
+		res.render(res.view, null, function(err, result) {
+			//console.log('Render result:', result);
+			// compact output by removing carriage returns, tabs and extra whitespace
+			var html = (this.options.debug) ? result : result.replace(/(\r\n|\n|\r|\t)/gm,"").replace(/\s+/gm, " ");
+			res.send(html); // send rendered HTML back to client
+		})
+	}, 
     ensureAuthenticated: function(req, res, next) {
 		if (req.isAuthenticated()) { return (next) ? next() : true; }
 		// always redirect to the homepage if not authenticated (customize?)
@@ -27,17 +37,6 @@ main = Class.extend({
     }
 	
 });
-
-main.prototype.render = function(res){
-	//console.log( path.join(__dirname, '/views/'+res.template) );
-	//res.render(res.view, { layout: path.join(__dirname, '/views/'+res.template) });
-	res.render(res.view, null, function(err, result) {
-		//console.log('Render result:', result);
-		// compact output by removing carriage returns, tabs and extra whitespace
-		var html = (this.options.debug) ? result : result.replace(/(\r\n|\n|\r|\t)/gm,"").replace(/\s+/gm, " ");
-		res.send(html); // send rendered HTML back to client
-	});
-}
 
 
 module.exports = main;
