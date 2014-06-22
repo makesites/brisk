ab31f6fdcd465138a4b86b6f3cf6b713ac0782e1
Makis Tracend <makis.tracend@gmail.com>
2014-06-21 21:40:58 -0700

    #48 defining template engine in (express) helper

--- app/helpers/express.js
+++ app/helpers/express.js
@@ -1,19 +1,21 @@
-var brisk = require("brisk"), 
+var brisk = require("brisk"),
 	Parent = require("./class"),
 	CORS = brisk.getLib("connect-xcors");
 
 var helper = Parent.extend({
-	
+
+	engine: false, // replace with your temaplte engine
+
 	//CORS middleware
 	cors : function() {
-		// load configuration 
+		// load configuration
 		return CORS( brisk.loadConfig('cors') );
-	}, 
-	
+	},
+
 	self: function() {
 		return this.express;
 	}
-	
+
 });
 
 
